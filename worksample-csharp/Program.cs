e0a6534ef3b9e2d042d40c5742066a7322bc835d
jon <kilpatrick.jonluc@outlook.com>
2022-03-25 17:18:23 -0400

    adding demo set

---
+++ worksample-csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MultiValueDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+                Console.Write(">");
+                string input = Console.ReadLine();
+                Console.WriteLine("You entered " + input);
+            }
+        }
+    }
+}
